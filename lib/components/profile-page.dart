d2f4bdc35aaeea56e730edcc7e4e6330d4832833
Umutcan Bostanci <umutcanbostancii@gmail.com>
2023-03-17 12:39:05 +0300

    Profile photos created, following, follower, and post sections created, message and follow/unfollow button created.

--- lib/components/profile-page.dart
+++ lib/components/profile-page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -39,28 +38,179 @@ class ProfilePage extends StatelessWidget {
         ),
         leadingWidth: 200,
       ),
-      body: Row(
+      body: Column(
         children: [
-          Stack(
-            children: const <Widget>[
-              Padding(
-                padding: EdgeInsets.all(12.0),
-                child: CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  radius: 51,
-                  child: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: 49,
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          "https://images.unsplash.com/photo-1558898479-33c0057a5d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"),
-                      radius: 47,
+          SizedBox(
+            height: 120,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Stack(
+                  children: const <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        radius: 51,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 49,
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://images.unsplash.com/photo-1558898479-33c0057a5d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"),
+                            radius: 47,
+                          ),
+                        ),
+                      ),
                     ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "87",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                      Text('Post')
+                    ],
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "824",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                      Text('Following')
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 17.0, right: 15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "1526",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                      Text('Follower')
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(left: 16.0),
+                child: Text(
+                  "Umutcan Bostanci",
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
                 ),
               ),
             ],
-          )
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(left: 15.9),
+                child: Text(
+                  "City/Istanbul",
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(left: 15.9),
+                child: Text(
+                  "Flutter/Swift",
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(left: 15.9),
+                child: Text(
+                  "Photographer",
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 9,
+          ),
+          SizedBox(
+            height: 30,
+            width: 370,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 150,
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Following",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left:8.0),
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Message",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 70,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left:8.0),
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: const Center(
+                        child:  Icon(
+                          Icons.person_off_sharp, color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
